90bebd26a3048042e0c9ddd41923a0b8c922f7b0
suhyunsim <cnc1106@naver.com>
2021-03-30 19:00:33 +0900

    refactor: simple-jekyll-search 기본 테마로 변경

--- assets/main.js
+++
@@ -1,5 +0,0 @@
-importJavaScript('simple-jekyll-search.min');
-
-function importJavaScript(scriptName) {
-    document.write('<script type="text/javascript" src="/assets/js/' + scriptName + '.js"></script>');
-}
